face32214b4f64cea528a098b5f457a1107cb0ab
Giulio Montaque <giuliom@hotmail.co.uk>
2024-08-15 23:30:25 +0000

    add wifi locations and address to wifi locations

---
+++ wifi_locations/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=150)
+    city = models.CharField(max_length=150)
+    country = models.CharField(max_length=150)
+    postcode = models.CharField(max_length=150)
+
+# Create your models here.
+class WifiLocation(models.Model):
+    name = models.CharField(max_length=50, blank=False)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
+    description = models.TextField()
+    star_rating = models.IntegerField()
+    amenities = models.CharField(max_length=50, blank=False)
+    added_by = models.ForeignKey(User, related_name='wifi_location', null=True, on_delete=models.SET_NULL)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
