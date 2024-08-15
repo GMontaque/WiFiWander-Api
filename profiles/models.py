ef06691153e4e2dcc5bf74ca2222414f0f080ba1
Giulio Montaque <giuliom@hotmail.co.uk>
2024-08-15 23:23:35 +0000

    create profile database with url links

---
+++ profiles/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    memorable_word = models.CharField(max_length=100, blank=True)
+    image = models.ImageField(upload_to='profile_images/', default='default.jpg')
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date_joined']
+
+    def __str__(self):
+        return self.user.username
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+post_save.connect(create_profile, sender=User)
---
+++ profiles/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Profile
+
+class ProfileSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context.get('request')
+        return request.user == obj.user
+
+    class Meta:
+        model = Profile
+        fields = ['id', 'user', 'memorable_word', 'image', 'date_joined', 'is_owner']
