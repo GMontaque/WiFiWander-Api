17c41052193a2afe06b2be31a99ee621bb5c9c8e
Giulio Montaque <giuliom@hotmail.co.uk>
2024-08-17 09:34:17 +0000

    refactor: update Location URL from 'location' to 'locations'

--- wifi_locations/urls.py
+++ wifi_locations/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from wifi_locations import views
 
 urlpatterns = [
-    path('wifi_location/', views.LocationList.as_view()),
-    path('wifi_location/<int:pk>/', views.Location.as_view()),
+    path('wifi_locations/', views.LocationList.as_view()),
+    path('wifi_locations/<int:pk>/', views.Location.as_view()),
     path('address/', views.AddressList.as_view()),
 ]
