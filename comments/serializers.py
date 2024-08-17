d8f74fcc6409cf99572ac13296d9b9416b3fb9ad
Giulio Montaque <giuliom@hotmail.co.uk>
2024-08-17 10:03:25 +0000

    fix: correct fields list in comments serializers

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,12 +1,15 @@
 from rest_framework import serializers
 from .models import Comments
+from wifi_locations.models import WifiLocation
 
 class CommentSerializer(serializers.ModelSerializer):
-    user = serializers.CharField(source='user.username')  
+    user = serializers.CharField(source='user.username')
+    wifi_location = serializers.PrimaryKeyRelatedField(queryset=WifiLocation.objects.all())
 
     class Meta:
         model = Comments
-        fields = ['user', 'comment_text', 'created_at']
+        fields = ['user', 'wifi_location', 'comment_text', 'created_at']
+
         # should wifi location be included even though it is not used
         '''
         so in the front end code under the comment section the user visiting 
