986ff67e616d476ae14cf3d11c55ab00eac721d8
Giulio Montaque <giuliom@hotmail.co.uk>
2024-08-19 10:32:29 +0000

    feat: add and install naturaltime

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,8 +1,17 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from .models import Comments
 
 class CommentSerializer(serializers.ModelSerializer):
     user = serializers.CharField(source='user.username', read_only=True)
+    created_at = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
 
     class Meta:
         model = Comments
