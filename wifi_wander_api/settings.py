f0245c01e24d2aa02878e7d56badf5b9fca3d788
Giulio Montaque <giuliom@hotmail.co.uk>
2024-08-19 16:20:31 +0000

    feat: update settings.py to work with react

--- wifi_wander_api/settings.py
+++ wifi_wander_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import re
 
 
 if os.path.exists('env.py'):
@@ -63,7 +64,13 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-gmontaque-wifiwanderapi-3n84qeqoapp.ws.codeinstitute-ide.net','localhost','wifi-wander-api-835560a3f6c2.herokuapp.com']
+ALLOWED_HOSTS = [
+    os.environ.get('ALLOWED_HOST'),
+    '8000-gmontaque-wifiwanderapi-3n84qeqoapp.ws.codeinstitute-ide.net',
+    'localhost',
+    'wifi-wander-api-835560a3f6c2.herokuapp.com',
+    '... .herokuapp.com',
+]
 
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-gmontaque-wifiwanderapi-3n84qeqoapp.ws.codeinstitute-ide.net',
@@ -116,6 +123,16 @@ if 'CLIENT_ORIGIN' in os.environ:
 else:
     CORS_ALLOWED_ORIGIN_REGEXES = [
         r"^https://.*\.gitpod\.io$",
+    ]
+
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
+    ]
+else:
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN')
     ]
 
 CORS_ALLOW_CREDENTIALS = True
