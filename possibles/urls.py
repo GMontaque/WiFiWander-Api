3d0e2cebeed24c80c7e12090285b2bc8eead6b2b
Giulio Montaque <giuliom@hotmail.co.uk>
2024-08-20 22:58:55 +0000

    refactor: removed incorrect possibles app

--- possibles/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from possibles import views
-
-urlpatterns = [
-    path('possibles/', views.PossiblesList.as_view()),
-    path('possibles/<int:pk>/', views.PossiblesDetail.as_view()),
-]
