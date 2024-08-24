f221818084ae71adb73f3e09d40c238ac33fd121
Giulio Montaque <giuliom@hotmail.co.uk>
2024-08-24 17:01:30 +0000

    bug: add null to image field in profile model

--- profiles/models.py
+++ profiles/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     memorable_word = models.CharField(max_length=100, blank=False)
-    image = models.ImageField(upload_to='images/', default='default_profile_q35ywj')
+    image = models.ImageField(upload_to='images/', default='default_profile_q35ywj', null=True)
 
     class Meta:
         ordering = ['user__username']
