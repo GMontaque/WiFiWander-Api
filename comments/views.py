99ccc38dd68ed6960a27ebf203812d4dbf461a77
Giulio Montaque <giuliom@hotmail.co.uk>
2024-08-30 11:44:39 +0000

    bug: correct issue with comments view authentication

--- comments/views.py
+++ comments/views.py
@@ -3,6 +3,7 @@ from django.http import Http404
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from .models import Comments
 from .serializers import CommentSerializer
 from wifi_wander_api.permissions import IsOwnerOrReadOnly
