15ac1e0405e4f9fae27012cf3d42deb323abbf4e
Giulio Montaque <giuliom@hotmail.co.uk>
2024-09-02 10:32:05 +0000

    refactor: update serialiser fields

--- favourites/serializers.py
+++ favourites/serializers.py
@@ -3,13 +3,21 @@ from .models import Favourites
 from wifi_locations.models import WifiLocation
 
 class FavouritesSerializer(serializers.ModelSerializer):
-    # check if user is required
     user = serializers.CharField(source='user.username', read_only=True)
-    wifi_location = serializers.PrimaryKeyRelatedField(queryset=WifiLocation.objects.all())  # Allow setting during creation
+    wifi_location_name = serializers.CharField(source='wifi_location.name', read_only=True)
+    wifi_location_image = serializers.ImageField(source='wifi_location.image', read_only=True)
+    wifi_location_id = serializers.PrimaryKeyRelatedField(
+        queryset=WifiLocation.objects.all(),
+        source='wifi_location',
+        write_only=True
+    )
     added_at = serializers.DateTimeField(read_only=True)
 
     class Meta:
         model = Favourites
-        fields = ['user', 'wifi_location', 'added_at','notes','folder_name','visit_status']
+        fields = [
+            'id', 'user', 'wifi_location_id', 'wifi_location_name', 
+            'wifi_location_image', 'added_at', 'notes', 'folder_name', 
+            'visit_status'
+        ]
         read_only_fields = ['user', 'added_at']
-
