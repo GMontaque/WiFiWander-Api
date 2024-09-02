93a5459580d00c32b96c1d7f421302e28ddc1951
Giulio Montaque <giuliom@hotmail.co.uk>
2024-09-02 16:01:39 +0000

    bug: update favourites serializer

--- favourites/serializers.py
+++ favourites/serializers.py
@@ -8,15 +8,16 @@ class FavouritesSerializer(serializers.ModelSerializer):
     wifi_location_image = serializers.ImageField(source='wifi_location.image', read_only=True)
     wifi_location_id = serializers.PrimaryKeyRelatedField(
         queryset=WifiLocation.objects.all(),
-        source='wifi_location',
+        source='wifi_location',  # Ensure this is linked to the related model
         write_only=True
     )
+    wifi_location_id_display = serializers.ReadOnlyField(source='wifi_location.id')  # Add this line
 
     class Meta:
         model = Favourites
         fields = [
-            'id', 'user', 'wifi_location_id', 'wifi_location_name',
-            'wifi_location_image', 'added_at', 'notes', 'folder_name',
+            'id', 'user', 'wifi_location_id_display', 'wifi_location_name', 
+            'wifi_location_image', 'added_at', 'notes', 'folder_name', 
             'visit_status'
         ]
         read_only_fields = ['user', 'added_at']
