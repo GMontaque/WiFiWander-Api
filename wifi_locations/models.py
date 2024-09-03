fea4f8f904eba41d0193554b6ee7db1796b00140
Giulio Montaque <giuliom@hotmail.co.uk>
2024-09-03 10:09:31 +0000

    feat: update wifi locations to include new field continents

--- wifi_locations/models.py
+++ wifi_locations/models.py
@@ -1,16 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+CONTINENT_CHOICES = [
+    ('Africa', 'Africa'),
+    ('Asia', 'Asia'),
+    ('Europe', 'Europe'),
+    ('North America', 'North America'),
+    ('Australia', 'Australia'),
+    ('South America', 'South America'),
+]
+
 class WifiLocation(models.Model):
     name = models.CharField(max_length=50, blank=False, unique=True)
-    street = models.CharField(max_length=150, blank=False,default='')
-    city = models.CharField(max_length=150, blank=False,default='')
-    country = models.CharField(max_length=150, blank=False,default='')
-    postcode = models.CharField(max_length=150, blank=False,default='')
+    street = models.CharField(max_length=150, blank=False, default='')
+    city = models.CharField(max_length=150, blank=False, default='')
+    country = models.CharField(max_length=150, blank=False, default='')
+    postcode = models.CharField(max_length=150, blank=False, default='')
     image = models.ImageField(upload_to='images/', default='../default_profile_q35ywj', null=True)
-    description = models.TextField(blank=False,default='')
-    amenities = models.CharField(max_length=50, blank=False,default='')
-    added_by = models.ForeignKey(User, related_name='wifi_locations', null=True, on_delete=models.SET_NULL,default='')
+    description = models.TextField(blank=False, default='')
+    amenities = models.CharField(max_length=50, blank=False, default='')
+    continent = models.CharField(max_length=50, choices=CONTINENT_CHOICES, blank=False, default='')
+    added_by = models.ForeignKey(User, related_name='wifi_locations', null=True, on_delete=models.SET_NULL, default='')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- wifi_locations/serializers.py
+++ wifi_locations/serializers.py
@@ -8,15 +8,15 @@ class WifiLocationSerializer(serializers.ModelSerializer):
     updated_at = serializers.SerializerMethodField()
     is_owner = serializers.SerializerMethodField()
 
-    # Method to get human-readable 'created_at'
+    # human-readable 'created_at'
     def get_created_at(self, obj):
         return naturaltime(obj.created_at)
 
-    # Method to get human-readable 'updated_at'
+    # human-readable 'updated_at'
     def get_updated_at(self, obj):
         return naturaltime(obj.updated_at)
 
-    # Method to determine if the request user is the owner of the object
+    # checks if request user is the owner of the object
     def get_is_owner(self, obj):
         request = self.context.get('request')
         return request.user == obj.added_by
@@ -33,6 +33,7 @@ class WifiLocationSerializer(serializers.ModelSerializer):
             'image', 
             'description', 
             'amenities', 
+            'continent',
             'added_by', 
             'created_at', 
             'updated_at',
