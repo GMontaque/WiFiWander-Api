1f50197e367f8c16bf9d8cd2a8d2a14ba620fe96
Giulio Montaque <giuliom@hotmail.co.uk>
2024-09-17 04:28:18 +0000

    chore: remove debug print statment

--- wifi_wander_api/settings.py
+++ wifi_wander_api/settings.py
@@ -179,7 +179,6 @@ else:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
-print('connected')
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
